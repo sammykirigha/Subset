3eb43bd86516c38f1134a1d4962dad85aa4e6075
sammykirigha <dkirigha18@gmail.com>
2021-07-09 18:34:24 +0300

    added more functions

--- index.js
+++ index.js
@@ -57,3 +57,52 @@ function subHash(arr1, arr2) {
 
 console.log(subHash(["a", "b", "c", "d", "e", "f"], ["b", "d", "f"]))
 //O(N)
+
+
+//find the intersect of two arrays
+
+function intersect(arr1, arr2) {
+    let result = [];
+    let hashTable = {};
+    for (let i = 0; i < arr1.length; i++){
+        hashTable[arr1[i]] = true;
+    }
+    for (let j = 0; j < arr2.length; j++){
+        if (hashTable[arr2[j]]) {
+            result.push(arr2[j])
+        }
+    }
+    return result;
+}
+
+console.log(intersect([1, 2, 3, 4, 5], [0, 2, 4, 6, 8]))
+
+//find a duplicate in the array
+function duplicate(array) {
+    let result = [];
+    for (let i = 0; i < array.length; i++){
+        for (let j = i + 1; j < array.length; j++){
+            if (array[i] === array[j]) {
+                result.push(array[i])
+            }
+        }
+    }
+    return result;
+}
+
+console.log(duplicate(["a", "b", "c", "d", "c", "e", "a"]))
+
+function duplicateHash(array) {
+    let hashTable = {};
+    for ( let i = 0; i < array.length; i++){
+        if (hashTable[array[i]]) {
+            return array[i]
+        } else {
+            hashTable[array[i]] = true;
+        }
+    }
+    return false
+}
+
+console.log(duplicateHash(["a", "b", "c", "d", "c","e"]))
+
